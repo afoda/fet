b14b3e5745dfcd388f07934f1b19d455c725a775
Adel Foda <adel..foda@gmail.com>
2014-09-16 22:04:02 +1000

    Add Yeoman's angular scaffolding

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name jsEartrainerNgApp
+ * @description
+ * # jsEartrainerNgApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('jsEartrainerNgApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name jsEartrainerNgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the jsEartrainerNgApp
+ */
+angular.module('jsEartrainerNgApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
