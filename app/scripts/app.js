7860d85f44e57b0ef3097c8303f101edb51e8323
Adel Foda <adel..foda@gmail.com>
2014-09-23 08:56:05 +1000

    Add routes and placeholder views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,6 +7,30 @@
  * # fetApp
  *
  * Main module of the application.
+ * Contains controllers for settings, sets, and questions.
  */
 angular
-  .module('fetApp', ['ui.router']);
+  .module('fetApp', ['ui.router'])
+
+  .config(["$urlRouterProvider", "$stateProvider", function($urlRouterProvider, $stateProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+      .state('setup', {
+        url: '/',
+        templateUrl: 'views/setup.html',
+      })
+
+      .state('playing', {
+        url: '/',
+        templateUrl: 'views/playing.html',
+      })
+
+      .state('finished', {
+        url: '/',
+        templateUrl: 'views/finished.html',
+      });
+
+  }]);
