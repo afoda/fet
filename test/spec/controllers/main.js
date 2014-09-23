faa92113a8eb8521b528f0ca936dc92a4a872d25
Adel Foda <adel..foda@gmail.com>
2014-09-23 23:26:22 +1000

    Update app name in scaffold controller test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -3,7 +3,7 @@
 describe('Controller: MainCtrl', function () {
 
   // load the controller's module
-  beforeEach(module('jsEartrainerNgApp'));
+  beforeEach(module('fetApp'));
 
   var MainCtrl,
     scope;
