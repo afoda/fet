072e2a5c7cf2c6ac7d6989f48e505707231e8b23
Adel Foda <adel..foda@gmail.com>
2014-10-02 23:10:41 +1000

    Add documentation for midi.js mock

--- test/mock/midi.js
+++ test/mock/midi.js
@@ -1,5 +1,13 @@
 'use strict';
 
+/**
+ * @ngdoc overview
+ * @name mock.midi
+ * @description
+ * # mock.midi
+ *
+ * Provides a mock of MIDI.js which doesn't have side effects.
+ */
 angular.module('mock.midi', [])
 
   .constant('MIDI', {
