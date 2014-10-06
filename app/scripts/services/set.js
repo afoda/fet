873c80cce9d3c3de9e98a81cfa9e9a64434efa40
Adel Foda <adel..foda@gmail.com>
2014-10-06 17:30:25 +1100

    Add set service

---
+++ app/scripts/services/set.js
@@ -0,0 +1,91 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name fetApp.set
+ * @description
+ * # set
+ * Stores the state of the running exercise.
+ */
+angular.module('fetApp')
+  .factory('set', ['musicTheory', 'settings', function (musicTheory, settings) {
+
+    var service = {};
+
+    service.resetExercise = function() {
+      service.question = {};
+      service.finished = false;
+      service.stats = {
+        currentQuestion: 0,
+        currentScore: 0
+      };
+      newQuestion();
+    }
+
+    function newQuestion() {
+
+      var changeKey = service.stats.currentQuestion % settings.cadenceInterval == 0;
+      if (changeKey)
+        randomizeKey();
+
+      service.question.notes = [];
+      service.question.degrees = [];
+
+      var scale = musicTheory.modeNotes(settings.getMode(), service.root, settings.low, settings.high);
+      var noteDegrees = _.sample(scale, settings.harmonicNotes);
+
+      for (var i = 0; i < noteDegrees.length; i++) {
+        service.question.notes.push(noteDegrees[i][0]);
+        service.question.degrees.push(noteDegrees[i][1]);
+      }
+
+      service.stats.currentQuestion += 1;
+
+      return changeKey;
+    }
+
+    function randomizeKey() {
+      var allKeys = _.range(settings.keyLow, settings.keyHigh + 1);
+      service.root = _.sample(allKeys);
+    }
+
+    function scoreAnswer(answer) {
+      function quotient(notes) {
+        // Identify equivalent notes
+        return _.map(notes, function (note) {
+          return note == 8 ? 1 : note;
+        });
+      }
+
+      function uniqueDegrees(notes) {
+        return _.uniq(quotient(notes));
+      }
+
+      var realAnswer = uniqueDegrees(service.question.degrees).sort();
+      var userAnswer = uniqueDegrees(answer).sort();
+
+      if (_.isEqual(realAnswer, userAnswer))
+        service.stats.currentScore += 1;
+    }
+
+    /**
+     * @ngdoc method
+     * @name submitAnswer
+     * @methodOf fetApp.set
+     * @param {Array} answer The list of scale degrees (1-8) chosen by the user.
+     * @returns {Boolean} Whether the key changed on generating the new question.
+     */
+    service.submitAnswer = function(answer) {
+      scoreAnswer(answer);
+
+      if (service.stats.currentQuestion != settings.questionCount) {
+        return newQuestion();
+      }
+      else {
+        service.finished = true;
+      }
+    }
+
+    return service;
+
+  }]);
---
+++ test/spec/services/set.js
@@ -0,0 +1,51 @@
+'use strict';
+
+describe('Service: set', function () {
+
+  beforeEach(module('fetApp'));
+
+  var set,
+    settings;
+
+  beforeEach(inject(function (_set_, _settings_) {
+    set = _set_;
+    set.resetExercise();
+    settings = _settings_;
+  }));
+
+  it("starts on question 1", function () {
+    expect(set.stats.currentQuestion).toBe(1);
+  });
+
+  it("moves to next question after answer is submitted", function () {
+    set.submitAnswer([1,2,3]);
+    expect(set.stats.currentQuestion).toBe(2);
+  });
+
+  it("keeps track of the current question", function () {
+    for (var i = 0; i < settings.questionCount / 2; i++)
+      set.submitAnswer([1,2,3]);
+    expect(set.stats.currentQuestion).toBe(settings.questionCount/2 + 1);
+  });
+
+  it("sets finished state after all questions are answered", function () {
+    for (var i = 0; i < settings.questionCount; i++)
+      set.submitAnswer([1,2,3]);
+    expect(set.finished).toBe(true);
+  });
+
+  it("increments score on correct answer", function () {
+    set.submitAnswer(set.question.degrees);
+    expect(set.stats.currentScore).toBe(1);
+  });
+
+  it("starts with the right number of notes in the question", function () {
+    expect(set.question.degrees.length).toBe(settings.harmonicNotes);
+    expect(set.question.notes.length).toBe(settings.harmonicNotes);
+  });
+
+  it("starts with an appropriate key", function () {
+    expect(set.root >= settings.keyLow && set.root <= settings.keyHigh).toBe(true);
+  });
+
+});
